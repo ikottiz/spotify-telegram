cc2a2fb775828a017fb9b87af5036e1f6079d6b2
ikottiz <71371826+ikottiz@users.noreply.github.com>
2024-02-03 16:19:49 +0600

    Create user_data.js

---
+++ user_data.js
@@ -0,0 +1,12 @@
+module.exports = {
+  spotify: {
+    client_id: "",
+    client_secret: "",
+    redirect_uri: "http://localhost:3000/callback"
+  },
+  telegram: {
+    bot_token: "",
+    channel_id: "",
+    message_id: ""
+  }
+};
